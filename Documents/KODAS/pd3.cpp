e47dff42c012f2379e73bc4725a4712a02c5be95
adrija <adrija.kalikaite3@gmail.com>
2026-04-27 12:34:23 +0300

    pabaiga pagaliau

--- Documents/KODAS/pd3.cpp
+++ Documents/KODAS/pd3.cpp
@@ -2,38 +2,119 @@
 #include <fstream>
 #include <string>
 #include <iomanip>
-#include <vector>
-
 using namespace std;
-
-struct menuitemtype {string menuitem; double menuprice;}; // padarom item string nes pavadinimas, double nes nesveikas skaicius kaina
-void getdata(menuitemtype sarasas[], int &kiekis);
-void showmenu(menuitemtype sarasas[], int kiekiai[], int kiekis);
-
-int main()
+struct patiekalas
 {
-    int kiekis = 0;
-    int kiekiai[20] = {0}; //nes db nulis uzsakyta
-    menuitemtype sarasas[20];
-    getdata(sarasas,kiekis);
-    showmenu(sarasas,kiekis);
+    string pavadinimas;
+    double kaina;
+};
+void getData(patiekalas sarasas[], int &kiekis);
+void showMenu(patiekalas sarasas[], int kiekis);
+void spausdinimas(patiekalas sarasas[], int kiekiai[], int kiekis);
 
-    int pasirinkimas;
-    cout << "Koks patiekalo numeris?";
-    while (cin >> pasirinkimas && pasirinkimas != 0)
-    {
-        if (pasirinkimas > 0 && pasirinkimas <= kiekis)
-        {
-            int n;
-            cout << "kiek porciju noresite? " << endl;
-            cin >> n;
-            kiekiai[pasirinkimas - 1]+= n;
-            cout << "dar noresite dar kazko?"; { else {cout <<"nera tokio patiekalo";
-            }
+int main() {
+    patiekalas sarasas[20];
+    int kiekis = 0;
+    int kiekiai[20] = {0};
+    getData(sarasas, kiekis);
+
+    while (true) {
+        cout << "\nKa norite daryti?" << endl;
+        cout << "1 - Rinktis patiekala" << endl;
+        cout << "2 - Baigti ir gauti saskaita" << endl;
+        cout << "Pasirinkimas: ";
+
+        int veiksmas;
+        cin >> veiksmas;
+
+        if (veiksmas == 2) {
+            break;
         }
-        printcheck(sarasas,kiekiai,kiekis);
-        return 0;
+
+        switch (veiksmas) {
+            case 1:
+                showMenu(sarasas, kiekis);
+                int nr, n;
+                cout << "irasykite patiekalo numeri: ";
+                cin >> nr;
+                if (nr > 0 && nr <= kiekis) {
+                    cout << "Kiek porciji norite? ";
+                    cin >> n;
+                    kiekiai[nr - 1] += n;
+                    cout << "Uzsakyta!" << endl;
+                } else {
+                    cout << "Tokio patiekalo nera:(" << endl;
+                }
+                break;
+
+            default:
+                cout << "Klaida" << endl;
+                break;
+        }
     }
 
+    spausdinimas(sarasas, kiekiai, kiekis);
 
+    return 0;
+}
+
+void getData(patiekalas sarasas[], int &kiekis) {
+    ifstream fd("menu.txt");
+    if (!fd) {
+        cout << "Nera meniu" << endl;
+        return;
+    }
+
+    kiekis = 0;
+    string pav;
+    double k;
+
+    while (true) {
+        if (!getline(fd, pav)) break;
+        if (pav == "") break;
+
+        fd >> k;
+        fd.ignore(1000, '\n');
+
+        sarasas[kiekis].pavadinimas = pav;
+        sarasas[kiekis].kaina = k;
+        kiekis++;
+    }
+
+    fd.close();
+}
+void showMenu(patiekalas sarasas[], int kiekis) {
+    cout << "\nSiandienos meniu:" << endl;
+    for (int i = 0; i < kiekis; i++) {
+        cout << i + 1 << ". " << sarasas[i].pavadinimas << " - " << fixed << setprecision(2) << sarasas[i].kaina << "eur" << endl;
+    }
+}
+
+void spausdinimas(patiekalas sarasas[], int kiekiai[], int kiekis) {
+    ofstream fr("kvitas.txt");
+    double suma = 0;
+
+    cout << "\n--- SASKAITA ---" << endl;
+    fr << "--- SASKAITA ---" << endl;
+
+    for (int i = 0; i < kiekis; i++) {
+        if (kiekiai[i] > 0) {
+            double tarpine = kiekiai[i] * sarasas[i].kaina;
+            suma += tarpine;
+            cout << kiekiai[i] << " vnt. " << sarasas[i].pavadinimas << " : " << fixed << setprecision(2) << tarpine << " Eur" << endl;
+            fr << kiekiai[i] << " vnt. " << sarasas[i].pavadinimas << " : " << fixed << setprecision(2) << tarpine << " Eur" << endl;
+        }
+    }
+
+    double pvm = suma * 0.21;
+    double viso = suma + pvm;
+
+    cout << "\nMokesciai: " << pvm << " Eur" << endl;
+    cout << "Iš viso: " << viso << " Eur" << endl;
+
+    fr << "\nMokesciai: " << pvm << " Eur" << endl;
+    fr << "Is viso: " << viso << " Eur" << endl;
+
+    fr.close();
+    cout << "\nSkanaus!" << endl;
 }
